c77585a5e2ca82c5fc495efeab5d1ae459c11413
Ivan Telleria <ivantelleria96@gmail.com>
2025-04-17 09:56:39 +0200

    Initial Commit

---
+++ iCalculator/iCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculatorApp.swift
+//  iCalculator
+//
+//  Created by Ivan Telleria on 17/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
